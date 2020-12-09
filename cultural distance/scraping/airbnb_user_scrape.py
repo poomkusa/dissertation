7168df24adebfbe86f4bf42564e9ef827e089c68
Poom Kusawat <poom.loveyou@hotmail.com>
2020-12-10 01:05:59 +0700

    ...

--- cultural distance/scraping/airbnb_user_scrape.py
+++ cultural distance/scraping/airbnb_user_scrape.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
@@ -12,12 +5,13 @@ import requests
 import browsercookie
 import re
 import feather
-#import sys
-#sys.setrecursionlimit(10000)
+import psutil
+# import sys
+# sys.setrecursionlimit(10000)
 #import time
 #from random import gauss
 
-data = pd.read_csv("/home/poom/Desktop/1.csv")
+data = pd.read_csv("C:/Users/ThisPC/Desktop/New folder/2.csv")
 data["country"] = np.nan
 data["pic"] = np.nan
 data["face_num"] = np.nan
@@ -35,31 +29,34 @@ error_count = 0
 rownum = len(data)
 #grab cookie from browser
 cj = browsercookie.firefox()
+ram = 0
 while i!=rownum:
     try:
         if(first_time==True):
             print("loop number: "+str(i)+"/"+str(rownum))
-        url = "https://www.airbnb.com/users/show/"+str(data.iloc[i,data.columns.get_loc("reviewer_id")])
+            print(psutil.virtual_memory().percent)
+        url = "https://www.airbnb.com/users/show/"+str(data.loc[i,"reviewer_id"])
         #ดึงข้อมูลจากเว็บที่เราสนใจ
         r = requests.get(url, cookies=cj)
         #แปลงเป็น type bs4.BeautifulSoup
         html_page = BeautifulSoup(r.text, "html.parser")
         # country = html_page.select_one(selector)
-        country = html_page.find(text=re.compile('Lives in'))
-        pic = html_page.select(picture_selector)
-        if(not isinstance(data.iloc[i,data.columns.get_loc("country")], str)):
+        country = str(html_page.find(text=re.compile('Lives in')))
+        pic = str(html_page.select(picture_selector)[0]["src"])        
+        if(not isinstance(data.loc[i,"country"], str)):
             # data.iloc[i,data.columns.get_loc("country")] = country.text
-            data.iloc[i,data.columns.get_loc("country")] = country
-        if(not isinstance(data.iloc[i,data.columns.get_loc("pic")], str)):
-            data.iloc[i,data.columns.get_loc("pic")] = pic[0]["src"]
+            data.loc[i,"country"] = str(country)
+        if(not isinstance(data.loc[i,"pic"], str)):
+            data.loc[i,"pic"] = str(pic)            
 #        delay = gauss(6, 2)
 #        time.sleep(delay)
         i+=1
         error_count = 0
-        first_time = True
-        if(i%100==0):
-            #data.to_pickle("/home/poom/Desktop/1.pkl")
-            feather.write_dataframe(data, "/home/poom/Desktop/1.feather")
+        first_time = True        
+        if(i%5000==0 or (psutil.virtual_memory().percent>95 and ram==0)):
+            feather.write_dataframe(data, "C:/Users/ThisPC/Desktop/New folder/2.feather") 
+            if(psutil.virtual_memory().percent>95):
+                ram = 1           
     #stop and move on the next loop after 5 errors    
     except Exception as e:
         error_count+=1
@@ -70,8 +67,9 @@ while i!=rownum:
             continue
         first_time = False
         print("---"+str(e))
+feather.write_dataframe(data, "C:/Users/ThisPC/Desktop/New folder/2.feather")
 #data.to_pickle("/home/poom/Desktop/dummy.pkl")
-#data = pd.read_pickle("/home/poom/Desktop/1.pkl")
+#data = pd.read_pickle("/home/poom/Desktop/dummy.pkl")
 
 #for finding the last scraped row when resuming the scrape
 data.index[data['country'].isnull() == False]
